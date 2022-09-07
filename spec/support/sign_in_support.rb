74997d57604223947dc1d23e170721b1895a8955
tkhstty <tkhstty@gmail.com>
2022-09-07 16:19:06 +0900

    Update sign_in_support.rb

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -4,6 +4,5 @@ module SignInSupport
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
     find('input[name="commit"]').click
-    expect(current_path).to eq root_path
   end
 end
